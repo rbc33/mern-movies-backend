ddfc53e8d99397dc2545fa0b2786bfa3a6b2668c
rbc33 <ricardobenthem@gmail.com>
2025-05-23 12:58:06 +0200

    backend ended for now

--- api/movies.controller.js
+++ api/movies.controller.js
@@ -1,27 +1,69 @@
-import MoviesDAO from "../dao/moviesDAO";
+import MoviesDAO from "../dao/moviesDAO.js";
 
 export default class MoviesController {
-  static async apiGetMovies(req, res, next) {
-    const moviesPerPage = req.query.moviesPerPage ? parseInt(req.query.moviesPerPage) : 20 
-    const page = req.query.page ? parseInt(req.query.page) : 0
-        
-    let filters = {}
-    if (req.query.rated) {
-      filters.rated = req.query.rated
-    } else if {
-      filters.title = req.query.title
-    }
-    const { moviesList, totalNumMovies } = await MoviesDAO.apiGetMovies({ filters, page, moviesPerPage })
-    let response = {
-      movies: moviesList,
-      page: page,
-      filters: filters,
-      entries_per_page: moviesPerPage,
-      total_results: totalNumMovies,
-    }
-    res.json(response)
-    
-        
-        
-  }
+	static async apiGetMovies(req, res, next) {
+		const moviesPerPage = req.query.moviesPerPage
+			? parseInt(req.query.moviesPerPage)
+			: 20;
+		const page = req.query.page ? parseInt(req.query.page) : 0;
+
+		let filters = {};
+		if (req.query.rated) {
+			filters.rated = req.query.rated;
+		} else if (req.query.title) {
+			filters.title = req.query.title;
+		}
+		try {
+			const movieRes = await MoviesDAO.getMovies({
+				filters,
+				page,
+				moviesPerPage,
+			});
+			if (!movieRes) {
+				throw new Error("No results returned from MoviesDAO");
+			}
+			let response = {
+				movies: movieRes.moviesList,
+				page: page,
+				filters: filters,
+				entries_per_page: moviesPerPage,
+				total_results: movieRes.totalNumMovies,
+			};
+			res.json(response);
+		} catch (e) {
+			console.error(`API Error: ${e.message}`);
+			res.status(500).json({ error: e.message });
+		}
+	}
+	static async apiGetMovieById(req, res, next) {
+		try {
+			let id = req.params.id || {};
+
+			let movie = await MoviesDAO.getMovieById(id);
+
+			if (!movie) {
+				res.status(404).json({ error: "not found" });
+
+				return;
+			}
+
+			res.json(movie);
+		} catch (e) {
+			console.log(`api, ${e}`);
+
+			res.status(500).json({ error: e });
+		}
+	}
+
+	static async apiGetRatings(req, res, next) {
+		try {
+			let propertyTypes = await MoviesDAO.getRatings();
+
+			res.json(propertyTypes);
+		} catch (e) {
+			console.log(`api,${e}`);
+
+			res.status(500).json({ error: e });
+		}
+	}
 }
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import app from "./server.js";
 import mongodb from "mongodb";
 import dotenv from "dotenv";
 import MoviesDAO from "./dao/moviesDAO.js";
+import ReviewsDAO from "./dao/reviewsDAO.js";
 
 async function main() {
 	dotenv.config();
@@ -11,11 +12,12 @@ async function main() {
 	const port = process.env.PORT || 8000;
 
 	try {
-		// Connect to the MongoDB cluster
 		await client.connect();
 		await MoviesDAO.injectDB(client);
+		await ReviewsDAO.injectDB(client);
 		app.listen(port, () => {
 			console.log("server is running on port:" + port);
+			console.log("http://localhost:" + port);
 		});
 	} catch (e) {
 		console.error(e);
